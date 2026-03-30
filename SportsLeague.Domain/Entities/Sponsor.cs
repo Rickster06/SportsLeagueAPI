6ff68a4f723b9e002d411358cbe1aa7b0b520c5f
MillerDB <millerberrio1116427@correo.itm.edu.co>
2026-03-30 15:51:41 -0500

    Creation of Sponsor Entitie

---
+++ SportsLeague.Domain/Entities/Sponsor.cs
@@ -0,0 +1,21 @@
+﻿using SportsLeague.Domain.Enums;
+
+namespace SportsLeague.Domain.Entities
+{
+    public class Sponsor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string ContactEmail { get; set; } = string.Empty;
+        public string? Phone { get; set; }
+        public string? WebsiteUrl { get; set; }
+
+        public SponsorCategory Category { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public ICollection<TournamentSponsor> TournamentSponsors { get; set; }
+            = new List<TournamentSponsor>();
+    }
+}
